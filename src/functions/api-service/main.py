b20c4e386d620b190213409595e3e7582dea07fb
shin4488 <efshinya48@gmail.com>
2025-07-14 22:49:17 +0900

    change: test

--- src/functions/api-service/main.py
+++ src/functions/api-service/main.py
@@ -3,5 +3,5 @@ import functions_framework
 @functions_framework.http
 def main(request):
     print(request)
-    return "Hello from Cloud Run!テスト"
+    return "Hello from Cloud Run!テスト1"
     # テスト
