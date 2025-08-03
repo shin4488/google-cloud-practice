cf4ef90cf7af2e5b1d4d3dab711934bd99e76b8f
shin4488 <efshinya48@gmail.com>
2025-08-03 09:22:15 +0900

    change: push docker image from actions to google cloud registory

--- src/functions/api-service/main.py
+++ src/functions/api-service/main.py
@@ -3,5 +3,5 @@ import functions_framework
 @functions_framework.http
 def main(request):
     print(request)
-    return "Hello from Cloud Run!テスト2"
+    return "Hello from Cloud Run!テスト3"
     # テスト
