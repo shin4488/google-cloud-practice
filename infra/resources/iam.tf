327d0be2eaa53034480c32c51780186e4590ba49
shin4488 <efshinya48@gmail.com>
2025-08-03 11:31:38 +0900

    change: description

--- infra/resources/iam.tf
+++ infra/resources/iam.tf
@@ -1,7 +1,7 @@
 # IAMの定義
 
 resource "google_project_iam_member" "user_owner" {
-  member  = "user:efshinya48@gmail.com"
-  project = "rising-abacus-465723-f5"
+  project = var.project_id
   role    = "roles/owner"
+  member  = "user:efshinya48@gmail.com"
 }
