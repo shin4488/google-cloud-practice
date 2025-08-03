92f381820cb58933d5c8dec966354b85d8feaee0
shin4488 <efshinya48@gmail.com>
2025-08-03 10:25:31 +0900

    change: reflect changes to source

--- src/functions/api-service/main.py
+++ src/functions/api-service/main.py
@@ -3,5 +3,5 @@ import functions_framework
 @functions_framework.http
 def main(request):
     print(request)
-    return "Hello from Cloud Run!テスト6"
+    return "Hello from Cloud Run!テスト7"
     # テスト
