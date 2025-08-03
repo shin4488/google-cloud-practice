afd92d61770b926c971a5b843acc9bfabb48b75b
shin4488 <efshinya48@gmail.com>
2025-08-03 09:45:57 +0900

    change: auth gcloud docker before build & push

--- src/functions/api-service/main.py
+++ src/functions/api-service/main.py
@@ -3,5 +3,5 @@ import functions_framework
 @functions_framework.http
 def main(request):
     print(request)
-    return "Hello from Cloud Run!テスト3"
+    return "Hello from Cloud Run!テスト4"
     # テスト
