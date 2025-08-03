f4403fc2428cf1abf98c8cf79a2f8955514cf60e
shin4488 <efshinya48@gmail.com>
2025-08-03 11:05:45 +0900

    change: not tu use source

--- src/functions/api-service/main.py
+++ src/functions/api-service/main.py
@@ -3,7 +3,7 @@ import functions_framework
 @functions_framework.http
 def main(request):
     print(request)
-    return "Hello from Cloud Run!テスト7"
+    return "Hello from Cloud Run!テスト8"
     # テスト
 
 @functions_framework.http
