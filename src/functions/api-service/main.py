34ee0385e7269335d0522f91d9377cbd1a7e9db0
shin4488 <efshinya48@gmail.com>
2025-08-03 11:01:50 +0900

    change: use base-image instead of source

--- src/functions/api-service/main.py
+++ src/functions/api-service/main.py
@@ -5,3 +5,8 @@ def main(request):
     print(request)
     return "Hello from Cloud Run!テスト7"
     # テスト
+
+@functions_framework.http
+def main2(request):
+    print("request2")
+    return "Hello from Cloud Run!2"
