eebd205e30792a2c4391462d131875bb061b3a96
shin4488 <efshinya48@gmail.com>
2025-10-13 07:43:01 +0000

    add: docker installation

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,3 +5,14 @@ set -euo pipefail
 # gcloud 設定ディレクトリの権限（named volume をマウントしているため）
 sudo mkdir -p /home/vscode/.config/gcloud/logs
 sudo chown -R vscode:vscode /home/vscode/.config/gcloud
+
+# # docker.sock の GID を取得
+# DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
+# # その GID のグループを用意（既存でもOK）
+# sudo groupadd -for -g "$DOCKER_GID" docker-host
+# # vscode をそのグループに追加
+# sudo usermod -aG docker-host "$USER"
+# # グループに書き込み権を付与
+# sudo chmod g+w /var/run/docker.sock
+
+sudo chmod o=rw /var/run/docker.sock
